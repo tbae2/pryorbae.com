2c95b2d4d4fc372e1cfcd5607af04bf353a8fb5a
Thomas Bae <thomas.bae0@gmail.com>
2015-03-26 23:21:23 -0400

    more updates to js, css and html

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,17 @@
 
 
-$(document).ready(function() {
+function jumboChange(page) {
 	
-	$(".jumbotron").css({ "background-image": "url('http://pryorbae.com/img/earlyShrimp.jpg')"});
 
+	switch (page)
+	{
+		case "home" :
+		$(".jumbotron").css({ "background-image": "url('/img/earlyShrimp.jpg')"}).delay(50).animate({opacity: 1}, 600);
+		break;
+		case "mtb" :
+		$(".jumbotron").css({ "background-image": "url('/img/mtb.jpg')"}).delay(250).animate({opacity: 1}, 600);
+		break;
+	
+	}
 
-});
+}
